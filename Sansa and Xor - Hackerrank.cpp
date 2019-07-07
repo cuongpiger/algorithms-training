419384f89647cab11f4890b7d737098bc87f7cca
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-07-07 23:54:26 +0700

    Sansa and Xor - Hackerrank

---
+++ Sansa and Xor - Hackerrank.cpp
@@ -0,0 +1,53 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <bitset>
+using namespace std;
+
+//=============================================================================================
+const double pi = 3.14159265359;
+#define p(a) ((a) * (a))
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> p2ll;
+typedef pair<string, int> psi;
+typedef pair<ld, ld> p2ld;
+typedef pair<char, char> pcc;
+//=============================================================================================
+
+/**
+ ** Comment **
+ **/
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+
+	int tc, n, x, res; cin >> tc;
+
+	while (tc--) {
+		res = 0;
+		cin >> n;
+
+		for (int i = 0; i < n; ++i) {
+			cin >> x;
+
+			if ((i + 1 )% 2) {
+				res ^= x;
+			}
+		}
+
+		if (n % 2) cout << res << endl;
+		else cout << 0 << endl;
+	}
+
+	return 0;
+}
