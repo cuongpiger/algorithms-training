8df1a9ac30c2ce1ffdd2f3813296e0cf976a7129
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-07-11 20:31:24 +0700

    Orange

--- MAKETREE - Hierarchy - SPOJ.cpp
+++ MAKETREE - Hierarchy - SPOJ.cpp
@@ -1,39 +1,39 @@
-#pragma warning(disable4996)
-#include iostream
-#include algorithm
-#include functional
-#include queue
-#include vector
+#pragma warning(disable:4996)
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <vector>
 using namespace std;
  
-=============================================================================================
+//=============================================================================================
 const double pi = 3.14159265359;
-#define p(a) ((a)  (a))
+#define p(a) ((a) * (a))
 typedef long long ll;
 typedef long double ld;
-typedef pairint, int pii;
-typedef pairdouble, double pdd;
-typedef pairll, ll p2ll;
-typedef pairstring, int psi;
-typedef pairld, ld p2ld;
-typedef pairchar, char pcc;
-=============================================================================================
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> p2ll;
+typedef pair<string, int> psi;
+typedef pair<ld, ld> p2ld;
+typedef pair<char, char> pcc;
+//=============================================================================================
  
 int students, successStudents;
  
-bool topologicalSort(vectorvectorint &grp, vectorint &res) {
-	vectorint inDegree(students + 1, 0);
-	queueint zeroInDegree;
+bool topologicalSort(vector<vector<int>> &grp, vector<int> &res) {
+	vector<int> inDegree(students + 1, 0);
+	queue<int> zeroInDegree;
  
-	for (int u = 1; u = students; ++u) {
-		vectorintiterator it;
+	for (int u = 1; u <= students; ++u) {
+		vector<int>::iterator it;
  
 		for (it = grp[u].begin(); it != grp[u].end(); ++it) {
-			++inDegree[it];
+			++inDegree[*it];
 		}
 	}
  
-	for (int i = 1; i = students; ++i) {
+	for (int i = 1; i <= students; ++i) {
 		if (!inDegree[i]) {
 			zeroInDegree.push(i);
 		}
@@ -43,18 +43,18 @@ bool topologicalSort(vectorvectorint &grp, vectorint &res) {
 		int u = zeroInDegree.front();
 		zeroInDegree.pop();
 		res.push_back(u);
-		vectorintiterator it;
+		vector<int>::iterator it;
  
 		for (it = grp[u].begin(); it != grp[u].end(); ++it) {
-			--inDegree[it];
+			--inDegree[*it];
  
-			if (!inDegree[it]) {
-				zeroInDegree.push(it);
+			if (!inDegree[*it]) {
+				zeroInDegree.push(*it);
 			}
 		}
 	}
  
-	for (int i = 1; i = students; ++i) {
+	for (int i = 1; i <= students; ++i) {
 		if (inDegree[i]) {
 			return false;
 		}
@@ -64,20 +64,20 @@ bool topologicalSort(vectorvectorint &grp, vectorint &res) {
 }
  
 int main() {
-	vectorvectorint grp;
-	vectorint res;
-	vectorint position;
+	vector<vector<int>> grp;
+	vector<int> res;
+	vector<int> position;
  
-	cin  students  successStudents;
+	cin >> students >> successStudents;
  
-	grp.assign(students + 1, vectorint());
+	grp.assign(students + 1, vector<int>());
 	position.resize(students + 1);
  
-	for (int wishes, i = 1; i = successStudents; ++i) {
-		scanf(%d, &wishes);
+	for (int wishes, i = 1; i <= successStudents; ++i) {
+		scanf("%d", &wishes);
  
-		for (int statements, j = 0; j  wishes; ++j) {
-			scanf(%d, &statements);
+		for (int statements, j = 0; j < wishes; ++j) {
+			scanf("%d", &statements);
 			grp[i].push_back(statements);
 		}
 	}
@@ -85,14 +85,14 @@ int main() {
 	if (topologicalSort(grp, res)) {
 		int pos = 0;
  
-		for (int i = 0; i  res.size(); ++i) {
+		for (int i = 0; i < res.size(); ++i) {
 			int student = res[i];
 			position[student] = pos;
 			pos = student;
 		}
  
-		for (int i = 1; i = students; ++i) {
-			printf(%dn, position[i]);
+		for (int i = 1; i <= students; ++i) {
+			printf("%d\n", position[i]);
 		}
 	}
  
