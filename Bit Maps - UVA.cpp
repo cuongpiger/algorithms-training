aa74a95c7d743e5d8598a54c93b4934535c42411
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-07-17 14:34:31 +0700

    run for string's length less than 50 characters

--- Bit Maps - UVA.cpp
+++ Bit Maps - UVA.cpp
@@ -62,29 +62,22 @@ void fromBtoD(int row, int col, int height, int width, int p){
 	}
 }
 
-void fromDtoB(int row, int col, int height, int width, int &ch, string s, int p){
+void fromDtoB(int row, int col, int height, int width, int &ch, string &s, int p){
 	if (ch >= s.length() || !width || !height) {
 		if (!width || !height) --ch;
 		return;
 	}
-	printf("row = %d, col = %d, height = %d, width = %d, ch = %d, s[ch] = %c, p = %d\n", row, col, height, width, ch, s[ch], p);
+	//printf("row = %d, col = %d, height = %d, width = %d, ch = %d, s[ch] = %c, p = %d\n", row, col, height, width, ch, s[ch], p);
 
 	if (s[ch] == '1'){
-		//int bit_1 = pow(2, c*1.0) - 1;
-		//int bit_1 = pow(2, p*1.0) - 1;
-		//int all_1 = pow(2, c*1.0) - 1;
-		//int bit_1 = (int)(pow(2, p*1.0) - 1) << (c - p);
-
 		int all_1 = pow(2, c*1.0) - 1;
 		int bit_1 = (int)(pow(2, width*1.0) - 1) << (p - 1);
 		bit_1 &= all_1;
 
 		for (int i = row; i < row + height; ++i){
-			//int tmp = (bit_1 >> (p - 1)) << (p - 1);
-			int tmp = bit_1;
-			printf("tmp = %d, screen[%d] = %d\n", tmp, i, screen[i]);
-			screen[i] += tmp;
-			printf("screen[%d] = %d\n\n", i, screen[i]);
+			//printf("bit_1 = %d, screen[%d] = %d\n", bit_1, i, screen[i]);
+			screen[i] += bit_1;
+			//printf("screen[%d] = %d\n\n", i, screen[i]);
 		}
 
 		return;
@@ -95,60 +88,67 @@ void fromDtoB(int row, int col, int height, int width, int &ch, string s, int p)
 		fromDtoB(row + height / 2 + height % 2, col, height / 2, width / 2 + width % 2, ++ch, s, p + width / 2);
 		fromDtoB(row + height / 2 + height % 2, col + width / 2 + width % 2, height / 2, width / 2, ++ch, s, p);
 	}
-	else {
-		cout << endl << endl;
-	}
 }
 
-int decimalToBinary(int N)
+inline string show_binary(unsigned int u, int num_of_bits)
 {
+	string a = "";
 
-	// To store the binary number 
-	ll B_Number = 0;
-	int cnt = 0;
-	while (N != 0) {
-		int rem = N % 2;
-		ll c = pow(10, cnt);
-		B_Number += rem * c;
-		N /= 2;
+	int t = pow(2, num_of_bits);   // t is the max number that can be represented
 
-		// Count used to store exponent value 
-		cnt++;
-	}
+	for (t; t > 0; t = t / 2)           // t iterates through powers of 2
+		if (u >= t){                // check if u can be represented by current value of t
+			u -= t;
+			a += "1";               // if so, add a 1
+		}
+		else {
+			a += "0";               // if not, add a 0
+		}
 
-	return B_Number;
+		return a;                     // returns string
 }
 
 int main() {
 	/*ios_base::sync_with_stdio(0);
 	cin.tie(0), cout.tie(0);*/
 	char func;
-	string s, cp;
 
 	while (cin >> func, func != '#'){
-		cin >> r >> c >> s;
+		string s;
+		cin >> r >> c;
+		cin.ignore();
+
+		while (s.length() < r * c) {
+			string tmp;
+			getline(cin, tmp);
+			s += tmp;
+		}
+		cout << endl;
+		cout << s << endl;
+
+		screen.clear();
+		screen.resize(r, 0);
+		cout << (func == 'B' ? "D" : "B") << right << setw(4) << r << right << setw(4) << c << endl;
 
 		if (func == 'B'){
-			screen.resize(r);
 			for (int i = 0; i < r; ++i){
-				s.copy((char *)cp.c_str(), c, i*c);
-				screen[i] = stoi(cp, nullptr, 2);
+				cout << s.substr(i*c, c) << endl;
+				screen[i] = stoi(s.substr(i*c, c), nullptr, 2);
 			}
 
 			fromBtoD(0, 0, r, c, 1);
+			cout << endl;
 		}
 		else{
 			int ch = 0;
-			screen.resize(r,0 /*pow(2, c*1.0) - 1*/);
 			fromDtoB(0, 0, r, c, ch, s, 1);
 
 			for (int i = 0; i < r; ++i){
-				cout << decimalToBinary(screen[i]);
+				cout << show_binary(screen[i], c - 1);
 			}
+			cout << endl;
 		}
 	}
-
-
 
 	return 0;
 }
