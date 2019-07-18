3c2e288b6e4c0d1086806a6049a3ebe15898f69f
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-07-18 14:27:07 +0700

    Divide and Conquer

--- Bit Maps - UVA.cpp
+++ Bit Maps - UVA.cpp
@@ -32,58 +32,58 @@ typedef pair<char, char> pcc;
  ** Comment **
  **/
 
-void fromBtoD(vector<vector<int>> &bm, int row_s, int col_s, int row_e, int col_e, int &cnt){
-	if (row_s >= row_e || col_s >= col_e) return;
+vector<vector<int>> bm;
 
-	int cntBit_0 = 0;
+inline int checkBitMap(int &row, int &col, int &height, int &width){
+	int sum = 0;
 
-	for (int i = row_s; i < row_e; ++i){
-		cntBit_0 += (count(bm[i].begin() + col_s, bm[i].begin() + col_e, 0));
+	for (int i = row; i < row + height; ++i){
+		for (int j = col; j < col + width; ++j){
+			sum += bm[i][j];
+		}
 	}
 
-	if (cnt > 0 && !(cnt % 50)){
-		cout << endl;
-	}
-	++cnt;
-
-	if (cntBit_0 == (col_e - col_s)*(row_e - row_s)){
-		cout << "0";
-	}
-	else if (!cntBit_0){
-		cout << "1";
-	}
-	else{
-		cout << "D";
-
-		int rowHalf = (row_s + row_e + 1) / 2;
-		int colHalf = (col_s + col_e + 1) / 2;
-		fromBtoD(bm, row_s, col_s, rowHalf, colHalf, cnt);
-		fromBtoD(bm, row_s, colHalf, rowHalf, col_e, cnt);
-		fromBtoD(bm, rowHalf, col_s, row_e, colHalf, cnt);
-		fromBtoD(bm, rowHalf, colHalf, row_e, col_e, cnt);
-	}
+	return sum;
 }
 
-void fromDtoB(vector<vector<int>> &bm, int row_s, int col_s, int row_e, int col_e){
-	if (row_s >= row_e || col_s >= col_e) return;
-	int ch = cin.get();
+string fromBtoD(int row, int col, int height, int width){
+	if (height <= 0 || width <= 0) return "";
 
-	if (ch == '0' || ch == '1'){
-		for (int i = row_s; i < row_e; ++i){
-			for (int j = col_s; j < col_e; ++j){
-				bm[i][j] = ch - '0';
-			}
+	int sum = checkBitMap(row, col, height, width);
+	if (!sum){
+		return "0";
+	}
+	else if (sum == width * height){
+		return "1";
+	}
+
+	int widthHalf = (width + 1) / 2;
+	int heightHalf = (height + 1) / 2;
+	return "D" + fromBtoD(row, col, heightHalf, widthHalf)
+		+ fromBtoD(row, col + widthHalf, heightHalf, width / 2)
+		+ fromBtoD(row + heightHalf, col, height / 2, widthHalf)
+		+ fromBtoD(row + heightHalf, col + widthHalf, height / 2, width / 2);
+}
+
+void fromDtoB(vector<string> &dest, int row, int col, int height, int width){
+	if (height <= 0 || width <= 0) return;
+
+	char ch; cin >> ch;
+
+	if (ch == '1'){
+		string strReplace(width, '1');
+		for (int i = row; i < row + height; ++i){
+			dest[i].replace(col, width, strReplace);
 		}
-
 		return;
 	}
-	else {
-		int rowHalf = (row_s + row_e + 1) / 2;
-		int colHalf = (col_s + col_e + 1) / 2;
-		fromDtoB(bm, row_s, col_s, rowHalf, colHalf);
-		fromDtoB(bm, row_s, colHalf, rowHalf, col_e);
-		fromDtoB(bm, rowHalf, col_s, row_e, colHalf);
-		fromDtoB(bm, rowHalf, colHalf, row_e, col_e);
+	else if (ch == 'D') {
+		int widthHalf = (width + 1) / 2;
+		int heightHalf = (height + 1) / 2;
+		fromDtoB(dest, row, col, heightHalf, widthHalf);
+		fromDtoB(dest, row, col + widthHalf, heightHalf, width / 2);
+		fromDtoB(dest, row + heightHalf, col, height / 2, widthHalf);
+		fromDtoB(dest, row + heightHalf, col + widthHalf, height / 2, width / 2);
 	}
 }
 
@@ -96,38 +96,35 @@ int main() {
 	while (cin >> func, func != '#'){
 		cin >> row >> col; cin.ignore();
 		cout << (func == 'B' ? "D" : "B") << right << setw(4) << row << right << setw(4) << col << endl;
-		
-		vector<vector<int>> bm(row, vector<int>(col));
+		bm.clear();
+		string res = "";
+
 		if (func == 'B'){
-			string input;
-			while (input.length() < row *col){
-				string tmp;
-				getline(cin, tmp);
-				input += tmp;
-			}
-
 			for (int i = 0; i < row; ++i){
+				vector<int> tmp;
 				for (int j = 0; j < col; ++j){
-					bm[i][j] = input[col *i + j] - '0';
+					char ch; cin >> ch;
+					tmp.push_back((int)(ch - '0'));
 				}
+				bm.push_back(tmp);
 			}
 
-			int cnt = 0; // count the number of characters was printed on a line
-			fromBtoD(bm, 0, 0, row, col, cnt);
-			cout << endl;
+			res = fromBtoD(0, 0, row, col);
 		}
 		else{
-			fromDtoB(bm, 0, 0, row, col);
-			for (int i = 0; i < row; ++i){
-				for (int j = 0; j < col; ++j){
-					if (i + j > 0 && !((col*i + j) % 50)){
-						cout << endl;
-					}
-					cout << bm[i][j];
-				}
-			}
+			vector<string> dest(row, string(col, '0'));
+			fromDtoB(dest, 0, 0, row, col);
 
-			cout << endl;
+			for (int i = 0; i < row; ++i){
+				res += dest[i];
+			}
+		}
+
+		for (int i = 0; i < (int)res.size(); ++i) {
+			cout << res[i];
+			if ((i + 1) % 50 == 0 || i == (int)res.size() - 1) {
+				cout << endl;
+			}
 		}
 	}
 
