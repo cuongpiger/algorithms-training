be9174d90806ecc01417c72c71e2b083cc776ae6
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-08-06 10:54:32 +0700

    Dynamic Programming

---
+++ COINS - Bytelandian gold coins - SPOJ.cpp
@@ -0,0 +1,84 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+//=============================================================================================
+#define pw(a) ((a) * (a))
+#define _x first
+#define _y second
+#define vit vector<int>::iterator
+#define _cin(X) int (X); cin >> X
+#define _FILE freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout)
+#define _FAST ios_base::sync_with_stdio(0); cin.tie(NULL), cout.tie(NULL)
+#define _i(X) (int)(X)
+const double pi = 3.14159265359;
+typedef uint64_t uit;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> p2ll;
+typedef pair<string, int> psi;
+typedef pair<ld, ld> p2ld;
+typedef pair<char, char> pcc;
+typedef vector<bool> vbool;
+typedef vector<int> vint;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvint;
+int _gcd(int a, int b) { return !b ? a : _gcd(b, a % b); }
+inline double _distance(const pii& a, const pii& b) { return sqrt(pw((double)(a._x - b._x)) + pw((double)(a._y - b._y))); }
+//=============================================================================================
+
+/**
+** Comment **
+**/
+
+ll dp(ll n, unordered_map<ll, ll> &store) {
+	ll a = n / 2, b = n / 3, c = n / 4;
+
+	if (a + b + c > n) {
+		if (!store[a]) {
+			store[a] = max(dp(a, store), a);
+		}
+
+		if (!store[b]) {
+			store[b] = max(dp(b, store), b);
+		}
+
+		if (!store[c]) {
+			store[c] = max(dp(c, store), c);
+		}
+
+		return store[a] + store[b] + store[c];
+	}
+
+	return n;
+}
+
+int main() {
+	_FAST;
+	unordered_map<ll, ll> store;
+	
+	ll n;
+	while (cin >> n) {
+		cout << dp(n, store) << endl;
+	}
+
+	return 0;
+}
