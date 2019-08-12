09257b4a79d464462ae103a9751c701ad90eacb2
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-08-12 21:04:50 +0700

    Modular Inverse

--- Examination Papers - GeeksForGeeks.cpp
+++ Examination Papers - GeeksForGeeks.cpp
@@ -36,6 +36,7 @@ int _gcd(int a, int b) { return !b ? a : _gcd(b, a % b); }
 inline double _distance(const pii& a, const pii& b) { return sqrt(pw((double)(a._x - b._x)) + pw((double)(a._y - b._y))); }
 //=============================================================================================
 
+//https://practice.geeksforgeeks.org/problems/examination-papers/0
 /**
 ** Comment **
 **/
