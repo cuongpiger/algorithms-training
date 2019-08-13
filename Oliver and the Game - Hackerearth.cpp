40b71b5754e67b2e4cb5e48ebbdc2efebddbea72
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-08-13 16:33:13 +0700

    DFS, Topological Sorting

--- Oliver and the Game - Hackerearth.cpp
+++ Oliver and the Game - Hackerearth.cpp
@@ -36,6 +36,7 @@ int _gcd(int a, int b) { return !b ? a : _gcd(b, a %b); }
 inline double _distance(const pii &a, const pii &b) { return sqrt(pw((double)(a._x - b._x)) + pw((double)(a._y - b._y))); }
 //=============================================================================================
 
+//https://www.hackerearth.com/practice/algorithms/graphs/topological-sort/practice-problems/algorithm/oliver-and-the-game-3/
 /**
 ** Comment **
 https://www.geeksforgeeks.org/topological-sorting-using-departure-time-of-vertex/
