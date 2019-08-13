c2b5380968395683f6d7a02aeb5dbf03bf83fcf9
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-08-13 16:37:26 +0700

    Modular Division

--- Ones - UVA.cpp
+++ Ones - UVA.cpp
@@ -33,6 +33,7 @@ int _gcd(int a, int b){ return !b ? a : _gcd(b, a %b); }
 inline double _distance(const pii &a, const pii &b){ return sqrt(pw((double)(a.x - b.x)) + pw((double)(a.y - b.y))); }
 //=============================================================================================
 
+//https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=1068
 /**
 ** Comment **
 https://www.mathblog.dk/uva-10127-ones/
