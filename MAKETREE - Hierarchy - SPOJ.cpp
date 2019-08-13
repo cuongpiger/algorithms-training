b2bf17d9a14a68020724c3d206f319f62313dae3
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-08-13 16:09:26 +0700

    Topological Sorting

--- MAKETREE - Hierarchy - SPOJ.cpp
+++ MAKETREE - Hierarchy - SPOJ.cpp
@@ -25,7 +25,9 @@ typedef pair<string, int> psi;
 typedef pair<ld, ld> p2ld;
 typedef pair<char, char> pcc;
 //=============================================================================================
- 
+
+//https://www.spoj.com/problems/MAKETREE/
+
 int students, successStudents;
  
 bool topologicalSort(vector<vector<int>> &grp, vector<int> &res) {
