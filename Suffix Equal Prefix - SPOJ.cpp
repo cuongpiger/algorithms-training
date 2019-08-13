7c7552da3cb04be18efcb7644aaf928a29a4c44a
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-08-13 19:29:11 +0700

    Hash Table, Prefix Sum Array

--- Suffix Equal Prefix - SPOJ.cpp
+++ Suffix Equal Prefix - SPOJ.cpp
@@ -14,7 +14,7 @@
 #include <sstream>
 using namespace std;
 
-//=============================================================================================
+//==============================================================================================
 #define pw(a) ((a) * (a))
 #define _x first
 #define _y second
@@ -39,7 +39,7 @@ typedef vector<ll> vll;
 typedef vector<vector<int>> vvint;
 int _gcd(int a, int b) { return !b ? a : _gcd(b, a % b); }
 inline double _distance(const pii& a, const pii& b) { return sqrt(pw((double)(a._x - b._x)) + pw((double)(a._y - b._y))); }
-//=============================================================================================
+//==============================================================================================
 
 //https://www.spoj.com/problems/SUFEQPRE/
 /**
