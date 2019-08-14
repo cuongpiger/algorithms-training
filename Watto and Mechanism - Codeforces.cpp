caafebf9d670f89cfaaaff2fff5e80d98c9f4c68
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-08-14 14:19:17 +0700

    Hash Table

--- Watto and Mechanism - Codeforces.cpp
+++ Watto and Mechanism - Codeforces.cpp
@@ -45,6 +45,7 @@ int _gcd(int a, int b) { return !b ? a : _gcd(b, a % b); }
 inline double _distance(const pii& a, const pii& b) { return sqrt(pw((double)(a._x - b._x)) + pw((double)(a._y - b._y))); }
 //=============================================================================================
 
+//https://codeforces.com/problemset/problem/514/C
 /**
 ** Comment **
 https://imgur.com/gallery/AQcgnzG
