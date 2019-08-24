6bb286468fdf82e804fd3a6a56ca4bffe9379387
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-08-24 15:28:13 +0700

    KMP

---
+++ MUH and Cube Walls - Codeforces.cpp
@@ -0,0 +1,139 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+//=========================================================================================================================
+#define pw(a) ((a) * (a))
+#define vit vector<int>::iterator
+#define _cin(X) int (X); cin >> X
+#define _FILE freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout)
+#define _FAST ios_base::sync_with_stdio(0); cin.tie(NULL), cout.tie(NULL)
+#define _i(X) (int)(X)
+const double pi = 3.14159265359;
+const long long mod = 1e9 + 7;
+typedef uint64_t uit;
+typedef long long ll;
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> p2ll;
+typedef pair<string, int> psi;
+typedef pair<ld, ld> p2ld;
+typedef pair<char, char> pcc;
+typedef pair<ll, string> plls;
+typedef vector<bool> vbool;
+typedef vector<int> vint;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<p2ll> vp2ll;
+typedef vector<vector<int>> vvint;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<vector<int>>> vvvint;
+typedef vector<vector<vector<long long>>> vvvll;
+typedef vector<vector<pii>> vvpii;
+int _gcd(int a, int b) { return !b ? a : _gcd(b, a % b); }
+inline double _distance(const pii& a, const pii& b) { return sqrt(pw((double)(a.first - b.first)) + pw((double)(a.second - b.second))); }
+//=========================================================================================================================
+
+//https://codeforces.com/problemset/problem/471/D
+/**
+** Comment **
+**/
+
+vint prefix;
+
+inline void KMPpreprocess(const vint& p) {
+	prefix = vint(p.size(), 0);
+	int m = p.size(), j = 0, i = 1;
+
+	while (i < m) {
+		if (p[i] == p[j]) {
+			prefix[i++] = ++j;
+		}
+		else {
+			if (j) {
+				j = prefix[j - 1];
+			}
+			else {
+				prefix[i++] = 0;
+			}
+		}
+	}
+}
+
+inline int KMPsearch(const vint& t, const vint& p) {
+	int n = t.size(), m = p.size(), i = 0, j = 0, cnt = 0;
+
+	while (i < n) {
+		if (p[j] == t[i]) {
+			++j, ++i;
+		}
+
+		if (j == m) {
+			++cnt;
+			j = prefix[j - 1];
+		}
+		else if (i < n && p[j] != t[i]) {
+			if (j) {
+				j = prefix[j - 1];
+			}
+			else {
+				++i;
+			}
+		}
+	}
+
+	return cnt;
+}
+
+int main() {
+	_FAST;
+	_cin(n); _cin(w);
+	int x, res = 0;
+	vint a = vint(n - 1), b = vint(w - 1);
+
+	cin >> x;
+	for (int tmp, i = 0; i < n - 1; ++i) {
+		cin >> a[i];
+		tmp = a[i];
+		a[i] -= x;
+		x = tmp;
+	}
+
+	cin >> x;
+	for (int tmp, i = 0; i < w - 1; ++i) {
+		cin >> b[i];
+		tmp = b[i];
+		b[i] -= x;
+		x = tmp;
+	}
+
+	if (w == 1) {
+		res = n;
+	}
+	else if (n > 1) {
+		KMPpreprocess(b);
+		res = KMPsearch(a, b);
+	}
+
+	cout << res << endl;
+	
+	return 0;
+}
