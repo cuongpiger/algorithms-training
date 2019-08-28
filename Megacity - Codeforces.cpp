aafe9c3491329182302f193518e05ff949a6b6d6
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-08-28 11:42:29 +0700

    Sorting

---
+++ Megacity - Codeforces.cpp
@@ -0,0 +1,87 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+//=========================================================================================================================
+#define pw(a) ((a) * (a))
+#define vit vector<int>::iterator
+#define _cin(X) int (X); cin >> X
+#define _FILE freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout)
+#define _FAST ios_base::sync_with_stdio(0); cin.tie(NULL), cout.tie(NULL)
+const double pi = 3.14159265359;
+const long long mod = 1e9 + 7;
+typedef uint64_t uit;
+typedef long long ll;
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<double, int> pdi;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> p2ll;
+typedef pair<string, int> psi;
+typedef pair<ld, ld> p2ld;
+typedef pair<char, char> pcc;
+typedef pair<ll, string> plls;
+typedef vector<bool> vbool;
+typedef vector<int> vint;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pdi> vpdi;
+typedef vector<p2ll> vp2ll;
+typedef vector<vector<int>> vvint;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<vector<int>>> vvvint;
+typedef vector<vector<vector<long long>>> vvvll;
+typedef vector<vector<pii>> vvpii;
+int _gcd(int a, int b) { return !b ? a : _gcd(b, a % b); }
+inline double _distance(const pii& a, const pii& b) { return sqrt(pw((double)(a.first - b.first)) + pw((double)(a.second - b.second))); }
+//=========================================================================================================================
+
+//https://codeforces.com/problemset/problem/424/B
+/**
+** Comment **
+**/
+
+#define distance first
+#define population second
+
+int main() {
+	_FAST;
+	_cin(n); _cin(popu);
+	vpdi citys(n);
+
+	for (int i = 0; i < n; ++i) {
+		pii tmp;
+		cin >> tmp.first >> tmp.second >> citys[i].population;
+		citys[i].distance = _distance(tmp, { 0, 0 });
+	}
+	sort(citys.begin(), citys.end());
+
+	for (int i = 0; i < n; ++i) {
+		popu += citys[i].population;
+
+		if (popu >= (int)1e6) {
+			cout << fixed << setprecision(7) << citys[i].distance << endl;
+			return 0;
+		}
+	}
+	cout << "-1" << endl;
+
+	return 0;
+}
