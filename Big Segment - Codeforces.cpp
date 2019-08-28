ae372974337d11cff9b7a1282362914db7560c61
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-08-28 11:15:50 +0700

    Implementation

---
+++ Big Segment - Codeforces.cpp
@@ -0,0 +1,90 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+//=========================================================================================================================
+#define pw(a) ((a) * (a))
+#define vit vector<int>::iterator
+#define _cin(X) int (X); cin >> X
+#define _FILE freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout)
+#define _FAST ios_base::sync_with_stdio(0); cin.tie(NULL), cout.tie(NULL)
+const double pi = 3.14159265359;
+const long long mod = 1e9 + 7;
+typedef uint64_t uit;
+typedef long long ll;
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> p2ll;
+typedef pair<string, int> psi;
+typedef pair<ld, ld> p2ld;
+typedef pair<char, char> pcc;
+typedef pair<ll, string> plls;
+typedef vector<bool> vbool;
+typedef vector<int> vint;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<p2ll> vp2ll;
+typedef vector<vector<int>> vvint;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<vector<int>>> vvvint;
+typedef vector<vector<vector<long long>>> vvvll;
+typedef vector<vector<pii>> vvpii;
+int _gcd(int a, int b) { return !b ? a : _gcd(b, a % b); }
+inline double _distance(const pii& a, const pii& b) { return sqrt(pw((double)(a.first - b.first)) + pw((double)(a.second - b.second))); }
+//=========================================================================================================================
+
+//https://codeforces.com/problemset/problem/242/B
+/**
+** Comment **
+**/
+
+int main() {
+	_FAST;
+	_cin(n);
+	int leftMost = 1e9 + 10, rightMost = -1, res = -1;
+	pii segment = { 0, 0 };
+	
+	for (int left, right, check = 0, i = 1; i <= n; ++i, check = 0) {
+		cin >> left >> right;
+
+		if (left <= leftMost) {
+			leftMost = left;
+			++check;
+		}
+
+		if (right >= rightMost) {
+			rightMost = right;
+			++check;
+		}
+
+		if (check == 2) {
+			res = i;
+			segment = make_pair(leftMost, rightMost);
+		}
+	}
+
+	if (segment.first != leftMost || segment.second != rightMost) {
+		res = -1;
+	}
+	cout << res << endl;
+
+	return 0;
+}
