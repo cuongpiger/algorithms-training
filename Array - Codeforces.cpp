259dbe6fd7ac2c676063f4dc9a1adc6264b13395
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2019-08-31 22:15:20 +0700

    Implementation, Two pointers

---
+++ Array - Codeforces.cpp
@@ -0,0 +1,93 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+//=========================================================================================================================
+#define pw(a) ((a) * (a))
+#define vit vector<int>::iterator
+#define FILE_IO freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(NULL), cout.tie(NULL)
+const double pi = 3.14159265359;
+const long long mod = 1e9 + 7;
+typedef uint64_t uit;
+typedef long long ll;
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<double, int> pdi;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> p2ll;
+typedef pair<string, int> psi;
+typedef pair<ld, ld> p2ld;
+typedef pair<char, char> pcc;
+typedef pair<ll, string> plls;
+typedef vector<bool> vbool;
+typedef vector<int> vint;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pdi> vpdi;
+typedef vector<p2ll> vp2ll;
+typedef vector<vector<int>> vvint;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<pii>> vvpii;
+typedef vector<vector<vector<int>>> vvvint;
+typedef vector<vector<vector<long long>>> vvvll;
+int _gcd(int a, int b) { return !b ? a : _gcd(b, a % b); }
+inline double _distance(const pii& a, const pii& b) { return sqrt(pw((double)(a.first - b.first)) + pw((double)(a.second - b.second))); }
+//=========================================================================================================================
+
+//http://codeforces.com/problemset/problem/224/B
+/**
+** Comment **
+**/
+
+int path[100010], a[100010];
+
+int main() {
+	FAST_IO;
+	int n, k;
+	cin >> n >> k;
+
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+	}
+
+	for (int cnt = 0, i = 0, j = 0; j < n; ++j) {
+		++path[a[j]];
+
+		if (path[a[j]] == 1) {
+			++cnt;
+		}
+
+		while (cnt == k) {
+			--path[a[i]];
+
+			if (!path[a[i]]) {
+				cout << i + 1 << " " << j + 1 << endl;
+				return 0;
+			}
+
+			++i;
+		}
+	}
+
+	cout << "-1 -1" << endl;
+	
+	return 0;
+}
