92255a971aa170e201e08de74b24381900350f64
Manh Cuong <cuongpigerr@gmail.com>
2022-04-07 22:50:53 +0700

    This is Manh Cuong's commit 🌻.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 |2|[A. Fashion in Berland](https://codeforces.com/problemset/problem/691/A)|Codeforces|[2.cpp](./solutions/2.cpp)|`logic`|
 |3|[A. Night at the Museum](https://codeforces.com/problemset/problem/731/A)|Codeforces|[3.cpp](./solutions/3.cpp)|`logic`|
 |4|[A. Bear and Game](https://codeforces.com/problemset/problem/673/A)|Codeforces|[4.cpp](./solutions/4.cpp)|`logic`|
+|5|[A. Arrays](https://codeforces.com/contest/572/problem/A)|Codeforces|[5.cpp](./solutions/5.cpp)|`sort`|
---
+++ solutions/5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+#define FILE_IO {freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);}
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(NULL), cout.tie(NULL)
+
+int main() {
+    FAST_IO;
+    // FILE_IO;
+
+    int nA, nB, k, m, maxA, minB, x;
+    
+    cin >> nA >> nB >> k >> m;
+    for (int i = 0; i < nA; ++i) {
+        cin >> x;
+
+        if (i == k - 1) maxA = x;
+    }
+
+    for (int i = 0; i < nB; ++i) {
+        cin >> x;
+
+        if (i == nB - m) minB = x;
+    }
+
+    cout << (maxA < minB ? "YES" : "NO");
+
+
+    return 0;
+}
