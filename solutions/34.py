45c479fd09d513c44e7226aeb84206f0badeade8
Manh Cuong <cuongpigerr@gmail.com>
2022-06-19 23:33:49 +0700

    This is Manh Cuong's commit 🌻.

--- solutions/34.py
+++ solutions/34.py
@@ -41,19 +41,16 @@ def solve():
         if N + M == 0:
             return
         
-        res = {}
         gates = []
         for i in range(N):
             for j in range(M):
                 if sea[i][j] == '.' and (i == 0 or j == 0 or i == N - 1 or j == M - 1):
                     gates.append((i, j))
                     
-        if len(gates) == 2:
-            if bfs(gates[0][0], gates[0][1], gates[1][0], gates[1][1], N, M, sea):
-                print("valid")
-                continue
-            
-        print("invalid")
+        if len(gates) == 2 and bfs(gates[0][0], gates[0][1], gates[1][0], gates[1][1], N, M, sea):
+            print("valid")
+        else:
+            print("invalid")
     
     
 # FILE_IO()
