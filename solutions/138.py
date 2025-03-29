4b1ae56afdc3263edf8fbde9cc49da0906db2914
Cuong. Duong Manh <cuongpigerr@gmail.com>
2025-03-29 13:53:33 +0700

    [chore] problem 139

--- solutions/138.py
+++ solutions/138.py
@@ -3,5 +3,17 @@ from typing import List
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
         meetings = sorted(meetings, key=lambda x: (x[0], x[1]))
+        print(meetings)
+        count = 0
+        current_day = 0
 
-        return 0
+        for meeting in meetings:
+            if current_day < meeting[0]:
+                count += meeting[0] - current_day - 1
+                current_day = meeting[1]
+            else:
+                current_day = max(meeting[1], current_day)
+
+        count += days - current_day
+
+        return count
