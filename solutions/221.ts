5497f730592f2769744ade2681c879dcbcbf5a23
Cuong. Duong Manh <cuongpigerr@gmail.com>
2025-12-02 20:57:09 +0700

    Add solution for problem 221

---
+++ solutions/221.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function countNodes(root: TreeNode | null): number {
+    if (root == null) {
+        return 0;
+    }
+
+    return 1 + countNodes(root.left) + countNodes(root.right);
+};
+
