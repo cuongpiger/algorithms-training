c1823ee7c111884d4463a162b66e69ff791aec45
Cuong. Duong Manh <40521173+cuongpiger@users.noreply.github.com>
2025-12-11 13:32:18 +0700

    Create 246.py

---
+++ solutions/246.py
@@ -0,0 +1,24 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        left = 1
+        right = n
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            val = guess(mid)
+
+            if val == 0:
+                return mid
+            elif val > 0:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return 0
