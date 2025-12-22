8f0fbb9f14efedfcb2c8c8bf416b92769db7dcea
Cuong. Duong Manh <cuongpigerr@gmail.com>
2025-12-22 23:53:10 +0700

    Add 283 solution

---
+++ solutions/283.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        cnt_absent = 0
+        cnt_late = 0
+        cnt_remaining = 0
+
+        for ch in s:
+            if ch == "L":
+                cnt_late += 1
+            else:
+                if cnt_late < 3:
+                    cnt_remaining += cnt_late
+                    cnt_late = 0
+
+                if ch == "A":
+                    cnt_absent += 1
+                else:
+                    cnt_remaining += 1
+
+        return cnt_absent < 2 and cnt_late < 3
