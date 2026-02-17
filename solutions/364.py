24e77505db81060aec90e066c70b55f9992d7a91
Cuong. Duong Manh <cuongpigerr@gmail.com>
2026-02-17 21:28:51 +0700

    solution 362

--- solutions/364.py
+++ solutions/364.py
@@ -1,5 +1,3 @@
-from typing import List
-
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         return [x for x in nums if x % 2 == 0] + [x for x in nums if x % 2 != 0]
